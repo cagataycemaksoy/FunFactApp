57c9a0d8f5bd648edcd35caa9ed12edc03ed4603
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-14 22:32:16 +0100

    updated UI

--- FunFactApp/ContentView.swift
+++ FunFactApp/ContentView.swift
@@ -10,34 +10,42 @@ import SwiftUI
 struct ContentView: View {
   @State private var showFact = false
   
-    var body: some View {
-      GeometryReader { geo in
-        let safeAreaOffset = geo.safeAreaInsets.top
-        VStack {
+  var body: some View {
+    GeometryReader { geo in
+      let safeAreaOffset = geo.safeAreaInsets.top
+      VStack {
+        ZStack {
           Circle()
             .fill(Color("buttonColor"))
-            .frame(width: !showFact ? geo.size.width - 100 : geo.size.width - 50)
-            .overlay {
-              if !showFact {
-                Text("FUN FACT!")
-                  .font(Font.custom("Futura", size: 50))
-                  .minimumScaleFactor(0.5)
-                  .foregroundStyle(Color("backgroundColor"))
-              }
-            }
-            .frame(maxWidth: .infinity, alignment: .center)
-            .offset(y: showFact ? (-geo.frame(in: .local).midY - safeAreaOffset) : 0)
-            .onTapGesture {
-              withAnimation(.smooth(duration: 0.5, extraBounce: 0.1)) {
-                showFact.toggle()
-              }
-            }
+          
+          if !showFact {
+            Text("FUN FACT!")
+              .font(Font.custom("Futura", size: 35))
+              .minimumScaleFactor(0.5)
+              .multilineTextAlignment(.center)
+              .foregroundStyle(Color("backgroundColor"))
+          }
+          
+          Image(systemName: showFact ? "chevron.down" : "chevron.up")
+            .font(.title)
+            .fontWeight(.medium)
+            .foregroundStyle(Color("backgroundColor"))
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+            .padding(.bottom)
         }
-        .frame(maxHeight: .infinity)
+        .frame(width: !showFact ? geo.size.width / 1.9: geo.size.width - 50, height: !showFact ? geo.size.width / 1.9: geo.size.width - 50)
+        .offset(y: showFact ? (-geo.frame(in: .local).midY - safeAreaOffset) : 0)
+        .onTapGesture {
+          withAnimation(.smooth(duration: 0.5, extraBounce: 0.1)) {
+            showFact.toggle()
+          }
+        }
       }
-      .background(Color("backgroundColor"))
-      
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+    .background(Color("backgroundColor"))
+    
+  }
 }
 
 #Preview {
