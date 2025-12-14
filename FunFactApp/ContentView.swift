213bb5a13fb91fcacbf8166335e932f1ca64d23e
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-14 19:44:14 +0100

    animation added

--- FunFactApp/ContentView.swift
+++ FunFactApp/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var showFact = false
+  
     var body: some View {
       GeometryReader { geo in
         let safeAreaOffset = geo.safeAreaInsets.top
@@ -18,6 +19,11 @@ struct ContentView: View {
             .frame(width: !showFact ? geo.size.width - 100 : geo.size.width - 50)
             .frame(maxWidth: .infinity, alignment: .center)
             .offset(y: showFact ? (-geo.frame(in: .local).midY - safeAreaOffset) : 0)
+            .onTapGesture {
+              withAnimation {
+                showFact.toggle()
+              }
+            }
         }
         .frame(maxHeight: .infinity)
       }
