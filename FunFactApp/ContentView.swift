1760e72a7b40234f968834eb0668630da7d7badb
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-14 19:59:18 +0100

    UI updated

--- FunFactApp/ContentView.swift
+++ FunFactApp/ContentView.swift
@@ -17,10 +17,18 @@ struct ContentView: View {
           Circle()
             .fill(Color("buttonColor"))
             .frame(width: !showFact ? geo.size.width - 100 : geo.size.width - 50)
+            .overlay {
+              if !showFact {
+                Text("FUN FACT!")
+                  .font(Font.custom("Futura", size: 50))
+                  .minimumScaleFactor(0.5)
+                  .foregroundStyle(Color("backgroundColor"))
+              }
+            }
             .frame(maxWidth: .infinity, alignment: .center)
             .offset(y: showFact ? (-geo.frame(in: .local).midY - safeAreaOffset) : 0)
             .onTapGesture {
-              withAnimation {
+              withAnimation(.smooth(duration: 0.5, extraBounce: 0.1)) {
                 showFact.toggle()
               }
             }
