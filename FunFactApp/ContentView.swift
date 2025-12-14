bb2bb5f479d963b8a29cf4be027920d59df3621a
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-14 19:27:54 +0100

    UI updated

--- FunFactApp/ContentView.swift
+++ FunFactApp/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var showFact = true
+  @State private var showFact = false
     var body: some View {
       GeometryReader { geo in
         let safeAreaOffset = geo.safeAreaInsets.top
@@ -17,8 +17,9 @@ struct ContentView: View {
             .fill(Color("buttonColor"))
             .frame(width: !showFact ? geo.size.width - 100 : geo.size.width - 50)
             .frame(maxWidth: .infinity, alignment: .center)
-            .offset(y: showFact ? (-geo.frame(in: .local).minY - safeAreaOffset) : 0)
+            .offset(y: showFact ? (-geo.frame(in: .local).midY - safeAreaOffset) : 0)
         }
+        .frame(maxHeight: .infinity)
       }
       .background(Color("backgroundColor"))
       
