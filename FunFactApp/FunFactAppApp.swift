5904305b4f80b983f0d3b22a94be23b0d51e866e
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-14 18:23:35 +0100

    Initial Commit

---
+++ FunFactApp/FunFactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunFactAppApp.swift
+//  FunFactApp
+//
+//  Created by Cem Aksoy on 14.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FunFactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
